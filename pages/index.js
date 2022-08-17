7b7654c8a7c2a480d0761a09188fdaeadc3f035b
Tomas de Priede <tomasdepriede@gmail.com>
2022-08-17 19:43:52 -0300

    Fixed images

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import minionPic from '../public/images/minion.jpeg'
 
 export default function Home() {
   return (
@@ -13,9 +14,10 @@ export default function Home() {
         <div>Image Local: </div>
         <div>
           <Image
-            src="https://i.pinimg.com/originals/31/fa/29/31fa295bfec41fce854b648a5e1e54b7.jpg"
-            width={200}
-            height={323}
+            src={minionPic}
+            //Next js automaticamente carga el tamaño de la imagen por nosotros
+            // width={}
+            // height={}
           />
         </div>
         <div>Image Remote: </div>
@@ -31,7 +33,7 @@ See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host
           <Image
             src="https://i.pinimg.com/originals/31/fa/29/31fa295bfec41fce854b648a5e1e54b7.jpg"
             width={200}
-            height={323}
+            height={323} // En este caso es obligatorio informar el ancho o el largo
           />
         </div>
       </main>
